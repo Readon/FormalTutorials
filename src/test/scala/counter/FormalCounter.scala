00c7f2028867ecc04f02586b0736a30e4b297b56
Xiao Yindong <xydarcher@uestc.edu.cn>
2023-06-24 13:25:59 +0000

    verification done.

--- src/test/scala/counter/FormalCounter.scala
+++ src/test/scala/counter/FormalCounter.scala
@@ -31,9 +31,11 @@ class FormalCounterTester extends SpinalFormalFunSuite {
           cover(dut.value === i)
         }
 
-        when(pastValidAfterReset && past(inc)){
+        when(pastValidAfterReset && past(inc) && dut.value < 10){
           assert(changed(dut.value))
         }
+
+        assert(dut.value <= 10 && dut.value >= 2)
       })
   }
   test("formal_tester") {
